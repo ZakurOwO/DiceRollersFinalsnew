bbe6cdae9ea9e0158d24ad90259e6a67dc2e11e3
ZakurOwO <johndanielnavarro130@gmail.com>
2025-05-24 11:25:15 +0800

    www

--- DicerollersFinals/Form1.cs
+++ DicerollersFinals/Form1.cs
@@ -10,50 +10,11 @@ using System.Windows.Forms;
 
 namespace DicerollersFinals
 {
-    public partial class FrontPage : Form
+    public partial class Form1: Form
     {
-        private int currentBalance;
-
-        public FrontPage(int balance)
+        public Form1()
         {
             InitializeComponent();
-            currentBalance = balance;
-        }
-
-
-
-        private void InputBet_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void BetButton_Click(object sender, EventArgs e)
-        {
-           
-            if (string.IsNullOrWhiteSpace(InputBet.Text))
-            {
-                MessageBox.Show("Please input your desired bet", "Input Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            {
-                if (int.TryParse(InputBet.Text, out int bet))
-                {
-                    if (bet >= 1 && bet <= 18)
-                    {
-                        Form2 form2 = new Form2(bet, currentBalance);
-                        form2.Show();
-                        this.Hide();
-                    }
-                    else
-                    {
-                        MessageBox.Show("1 - 18 only", "Invalid Bet", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Please enter a valid number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
         }
     }
 }
